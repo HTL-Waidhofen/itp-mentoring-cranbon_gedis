80a32901b67e157a009544b13d3abd8bfbe6554d
BerchtoldLaurenz <laurenz.berchtold@htlwy.at>
2023-11-30 09:53:09 +0100

    made IsValidEmail and IsCorrectPassword

--- WPF_Mentoring/Classes/Authentification.cs
+++ WPF_Mentoring/Classes/Authentification.cs
@@ -2,11 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Text.RegularExpressions;
 
 namespace WPF_Mentoring.Classes
 {
-    internal class Authentification
+    public class Authentification
     {
+        public static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$";
+            return Regex.IsMatch(email, pattern); 
+        }
+        public bool IsCorrectPassword(string password,string email)
+        {
+            string connectionString = "Data Source=Mentoring.db;Version=3;";
+
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand("SELECT password FROM Users WHERE email = @Email", connection))
+                {
+                    command.Parameters.AddWithValue("@Email", email);
+
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string correctPassword = reader.GetString(2);
+                            return password == correctPassword;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
     }
+    
 }
