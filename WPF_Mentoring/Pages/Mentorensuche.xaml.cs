47e9a21538d132745aa58f7bc79c550ba4844cdf
Forstner Kevin <kevin.forstner@htlwy.at>
2024-02-01 09:28:00 +0100

    databank

--- WPF_Mentoring/Pages/Mentorensuche.xaml.cs
+++ WPF_Mentoring/Pages/Mentorensuche.xaml.cs
@@ -23,82 +23,34 @@ namespace WPF_Mentoring.Pages
     public partial class Mentorensuche : Page
     {
         public static MainWindow main;
-        public ObservableCollection<DeinDatenTyp> MeineDaten { get; set; }
+            public ObservableCollection<Person> DeineObservableCollection { get; set; }
 
         public Mentorensuche()
         {
             InitializeComponent();
-            MeineDaten = new ObservableCollection<DeinDatenTyp>();
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert1", Abteilung = "Wert2" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-            MeineDaten.Add(new DeinDatenTyp { Name = "Wert3", Abteilung = "Wert4" });
-
-            MeineDataGrid.ItemsSource = MeineDaten;
             main.createNav();
-
+            DeineObservableCollection = new ObservableCollection<Person>
+        {
+            new Person { Name = "Max", Fach = "Sew" , Klasse="3"},
+            new Person { Name = "Max", Fach = "Sew" , Klasse="3" },
+            new Person { Name = "Max", Fach = "Sew" , Klasse="3" }
+        };
+            DataGridMentor.Items.Add(DeineObservableCollection);
+            // Setze die Datenquelle der DataGrid
         }
-        public class DeinDatenTyp
+
+        public class Person
         {
             public string Name { get; set; }
-            public string Abteilung { get; set; }
-            public string Email { get; set; }
-            List<Mentor> mitarbeiterListe = new List<Mentor>
-            {
-                //New machen klasse Mitarbeiter gibts ned
-            //new Mentor { Name = "Max Mustermann", Stufe = "Senior", Abteilung = "Entwicklung", Email = "max.mustermann@example.com" }
-                
-            };
-            public string AusgewählterWert { get; set; }
+            public string Fach { get; set; }
+            public string Klasse { get; set; }
+            // Weitere Eigenschaften hinzufügen, falls benötigt
         }
 
-        private void SortierenButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Liste sortieren daten fehlt datentyp
-            //daten = new ObservableCollection<string>(daten.OrderBy(x => x));
-        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
         }
-        /* using System;
-using System.Collections.ObjectModel;
-using System.Windows;
-using System.Windows.Controls;
-
-public partial class MainWindow : Window
-{
-    public ObservableCollection<DeinDatenTyp> MeineDaten { get; set; }
-
-    public MainWindow()
-    {
-        InitializeComponent();
-
-        MeineDaten = new ObservableCollection<DeinDatenTyp>();
-        MeineDaten.Add(new DeinDatenTyp { Eigenschaft1 = "Wert1", Eigenschaft2 = "Wert2" });
-        MeineDaten.Add(new DeinDatenTyp { Eigenschaft1 = "Wert3", Eigenschaft2 = "Wert4" });
-
-        MeineDataGrid.ItemsSource = MeineDaten;
-    }
-
-    private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-    {
-        if (MeineDataGrid.SelectedItem != null)
-        {
-            // Hier kannst du auf den ausgewählten Wert zugreifen und speichern.
-            DeinDatenTyp ausgewaehlteDaten = (DeinDatenTyp)MeineDataGrid.SelectedItem;
-
-            // Beispiel: Zeige eine MessageBox mit dem ausgewählten Wert an.
-            MessageBox.Show($"Ausgewählter Wert: {ausgewaehlteDaten.Eigenschaft1}");
-        }
-    }
-}
-*/
-
     }
 }
 
